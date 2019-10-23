05665ead7c07cbaad43b23ba9207cdd2fb451436
d-gallagher <jibisjib@gmail.com>
2019-10-23 21:08:09 +0100

    Client, Server and Service running

--- GRPC-ClientServer/src/main/java/ie/gmit/ds/Client.java
+++ GRPC-ClientServer/src/main/java/ie/gmit/ds/Client.java
@@ -1,49 +1,120 @@
 package ie.gmit.ds;
 
+import com.google.protobuf.BoolValue;
+import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
+/*  Adapted from Adapted from async lab:
+    https://github.com/john-french/distributed-systems-labs/tree/master/grpc-async-inventory
+*/
 
 public class Client {
 
     private static final Logger logger =
             Logger.getLogger(Client.class.getName());
     private final ManagedChannel channel;
-    private final PasswordServiceGrpc.PasswordServiceStub asyncInventoryService;
-    private final PasswordServiceGrpc.PasswordServiceBlockingStub syncInventoryService;
+    private final PasswordServiceGrpc.PasswordServiceStub asyncPasswordService;
+    private final PasswordServiceGrpc.PasswordServiceBlockingStub syncPasswordService;
+
+    // User Input
+    Scanner userInput = new Scanner(System.in);
+
+    // Member variables to hold user input
+    private String userPassword;
+    private ByteString hashedPassword;
+    private ByteString salt;
+    private int userId;
+
 
     public Client(String host, int port) {
         channel = ManagedChannelBuilder
                 .forAddress(host, port)
                 .usePlaintext()
                 .build();
-        syncInventoryService = PasswordServiceGrpc.newBlockingStub(channel);
-        asyncInventoryService = PasswordServiceGrpc.newStub(channel);
+        syncPasswordService = PasswordServiceGrpc.newBlockingStub(channel);
+        asyncPasswordService = PasswordServiceGrpc.newStub(channel);
     }
 
     public void shutdown() throws InterruptedException {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-
     }
 
-    /*
-        Need to get user input for id/pw as in specs
-        > store in variables for validation
-        Need to perform pw validation
-        Client will call user input and validation in main
-     */
+    public void getUserInput(){
+        System.out.println("Enter ID:");
+        userId = userInput.nextInt();
+        System.out.println("Enter Password:");
+        userPassword = userInput.next();
+    }
+
+    public void sendHashRequest(){
+        getUserInput();
+
+        // Build HashRequest
+        System.out.println("Sending Hash Req");
+        HashRequest hashRequest = HashRequest.newBuilder().setUserId(userId)
+                                    .setPassword(userPassword).build();
+        // HashResponse
+        HashResponse hashResponse;
+
+        try {
+            hashResponse = syncPasswordService.hash(hashRequest);
+            hashedPassword = hashResponse.getHashedPassword();
+            salt = hashResponse.getSalt();
+        }catch (StatusRuntimeException ex){
+            logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
+            return;
+        }
+    }
+
+    public void sendValidationRequest(){
+        StreamObserver<BoolValue> responseObserver = new StreamObserver<BoolValue>(){
+            @Override
+            public void onNext(BoolValue value) {
+                if(value.getValue()){
+                    System.out.println("Auth Successful");
+                }
+                else{
+                    System.out.println("Username or Password is incorrect");
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("An Error has occurred.. "+t.getLocalizedMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.exit(0);
+            }
+        };
+        try {
+            asyncPasswordService.validate(ValidationRequest.newBuilder().setPassword(userPassword)
+                    .setHashedPassword(hashedPassword)
+                    .setSalt(salt).build(), responseObserver);
+        } catch (StatusRuntimeException ex) {
+            logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
+            return;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Client client = new Client("localhost", 50551);
-//        Item newItem = Item.newBuilder()
-//                .setId("1234")
-//                .setName("New Item")
-//                .setDescription("Best New Item")
-//                .build();
         try {
-//            client.addNewInventoryItem(newItem);
-//            client.getItems();
+            client.sendHashRequest();
+            client.sendValidationRequest();
+//            System.out.println("PW: "+ client.userPassword + "\nPWhash: "
+//                    +client.hashedPassword.toByteArray().toString()
+//                    + "\nPWsalt: "+client.salt.toByteArray().toString());
+
         } finally {
             // Don't stop process, keep alive to receive async response
             Thread.currentThread().join();
--- GRPC-ClientServer/src/main/java/ie/gmit/ds/PasswordServiceImpl.java
+++ GRPC-ClientServer/src/main/java/ie/gmit/ds/PasswordServiceImpl.java
@@ -1,7 +1,62 @@
 package ie.gmit.ds;
 
-
+import com.google.protobuf.BoolValue;
+import com.google.protobuf.ByteString;
+import io.grpc.stub.StreamObserver;
 
 public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImplBase {
+    /*  Adapted from Adapted from async lab:
+        https://github.com/john-french/distributed-systems-labs/tree/master/grpc-async-inventory
+        This class overrides the hash and validate methods provided in the proto file
+    */
+    PasswordServiceImpl(){}
 
+    @Override
+    public void hash(HashRequest request, StreamObserver<HashResponse> responseObserver){
+        try{
+            // Request User Provided Pw, send to char array
+            String userPassword = request.getPassword();
+            char[] stringToHash = userPassword.toCharArray();
+
+            // Passwords class is using byte arrays to generate salt & hash user pw
+            byte[] addSaltToHash = Passwords.getNextSalt();
+            byte[] hashedPassword = Passwords.hash(stringToHash, addSaltToHash);
+
+            // https://stackoverflow.com/questions/29018411/google-protobuf-bytestring-vs-byte
+            // Copy hashed pw and salt byte[] to immutable ByteStrings
+            // Response is built using generated protobuff classes
+            responseObserver.onNext(HashResponse.newBuilder().setUserId(request.getUserId())
+                    .setHashedPassword(ByteString.copyFrom(hashedPassword))
+                    .setSalt(ByteString.copyFrom(addSaltToHash))
+                    .build());
+        }
+        //send default if there is a problem
+        catch(RuntimeException ex){
+            responseObserver.onNext(HashResponse.newBuilder().getDefaultInstanceForType());
+        }
+        responseObserver.onCompleted();
+    }//end hash
+
+    @Override
+    public void validate(ValidationRequest request, StreamObserver<BoolValue> responseObserver){
+        try{
+            // isExpectedPassword(char[] password, byte[] salt, byte[] expectedHash)
+            char[] userPassword = request.getPassword().toCharArray();
+            byte[] hashedPassword = request.getHashedPassword().toByteArray();
+            byte[] salt = request.getSalt().toByteArray();
+
+            // Validate the hash matches and return true
+            if(Passwords.isExpectedPassword(userPassword, salt, hashedPassword)){
+                responseObserver.onNext(BoolValue.newBuilder().setValue(true).build());
+            }
+            // Else return false
+            else{
+                responseObserver.onNext(BoolValue.newBuilder().setValue(false).build());
+            }
+        }
+
+        catch(RuntimeException ex){
+            responseObserver.onNext(BoolValue.newBuilder().setValue(false).build());
+        }
+    }
 }
